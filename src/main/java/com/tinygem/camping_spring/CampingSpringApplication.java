12727b3f98b96175f239bf3d83b54f753f28f1fb
sarumani <qkre2018@gmail.com>
2023-09-11 17:55:51 +0900

    Initial commit

---
+++ src/main/java/com/tinygem/camping_spring/CampingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tinygem.camping_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingSpringApplication.class, args);
+    }
+
+}
